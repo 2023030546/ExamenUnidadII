0ee8e29c4590ae397755cae2262ef0cd38b6a32b
2023030546 <2023030546@upsin.edu.mx>
2025-10-24 14:11:19 -0700

    final

---
+++ js/Cotizacion.js
@@ -0,0 +1,22 @@
+const valor_auto = document.getElementById("valor_auto")
+const planes_credito = document.getElementById("planesCredito")
+const enganche = document.getElementById("enganche")
+const pago_inicial = document.getElementById("pagoini")
+const pago_financiar = document.getElementById("total_financiar")
+const pago_mesual = document.getElementById("pago_mensual")
+
+function calculo(){
+    const meses = parseInt(planes_credito.value)
+    const enga = parseFloat(enganche.value)
+    const valor = parseFloat(valor_auto.value)
+
+    let pagoini = valor - (valor*enga);
+    let pagofina = valor - pagoini;
+    let pagomensu = pagofina / meses;
+
+    pago_inicial.value = pagoini;
+    pago_financiar.value = pagofina;
+    pago_mesual.value = pagomensu;
+
+
+}
